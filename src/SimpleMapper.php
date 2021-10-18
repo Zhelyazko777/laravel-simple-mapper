0e6c90c4ab3ea7f0bb3551479f3e64252c8b3f53
zhelyazko777 <zhelyzko@abv.bg>
2021-10-18 13:05:48 +0300

    Simplified mapper

--- src/SimpleMapper.php
+++ src/SimpleMapper.php
@@ -4,10 +4,32 @@ namespace Zhelyazko777\LaravelSimpleMapper;
 
 class SimpleMapper
 {
-    public function map(object $source, object $destination): object
+    public static function map(object $source, object $destination): object
     {
-        /** @var SimpleMapperServiceInterface $service */
-        $service = app()->get(SimpleMapperServiceInterface::class);
-        return $service->map($source, $destination);
+        $sourceGetterNames = array_map(
+            fn ($getter) => lcfirst(preg_replace('/^get/', '', $getter)),
+            array_filter(
+                get_class_methods($source),
+                fn ($m) => str_starts_with($m, 'get')
+            )
+        );
+
+        $destinationSetterNames = array_map(
+            fn ($setter) => lcfirst(preg_replace('/^set/', '', $setter)),
+            array_filter(
+                get_class_methods($destination),
+                fn ($m) => str_starts_with($m, 'set')
+            )
+        );
+        foreach ($destinationSetterNames as $setter)
+        {
+            if (in_array($setter, $sourceGetterNames)) {
+                $setterFullName = 'set'.ucfirst($setter);
+                $getterFullName = 'get'.ucfirst($setter);
+                $destination->{$setterFullName}($source->{$getterFullName}());
+            }
+        }
+
+        return $destination;
     }
 }
--- src/SimpleMapperServiceInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Zhelyazko777\LaravelSimpleMapper;
-
-interface SimpleMapperServiceInterface
-{
-    public function map(object $source, object $destination): object;
-}
